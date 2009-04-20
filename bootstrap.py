ee70641d7320eea5b03235d24f2938569a4a251e
Anders Pearson <anders@columbia.edu>
2009-04-20 13:44:24 -0400

    converted to pip

--- bootstrap.py
+++ bootstrap.py
@@ -10,5 +10,8 @@ vedir = os.path.join(pwd,"ve")
 if os.path.exists(vedir):
     shutil.rmtree(vedir)
 
-subprocess.call(["python",os.path.join(pwd,"create-ve.py")])
-subprocess.call(["python",os.path.join(pwd,"ve-bootstrap.py"),vedir])
+subprocess.call([os.path.join(pwd,"pip.py"),"install",
+                 "-E",os.path.join(pwd,"ve"),
+                 "--enable-site-packages",
+                 "--requirement",os.path.join(pwd,"requirements/apps.txt")])
+
