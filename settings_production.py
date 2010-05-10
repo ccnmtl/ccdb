46c2ff9fd71f850bddd72a42ca40e92e4ca68702
Anders Pearson <anders@columbia.edu>
2010-05-10 10:55:14 -0400

    switch to fs cache and up the fragment cache timeout

--- settings_production.py
+++ settings_production.py
@@ -9,5 +9,5 @@ MEDIA_ROOT = '/var/www/ccdb/uploads/'
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 #CACHE_BACKEND = 'locmem:///'
-CACHE_BACKEND = 'johnny.backends.locmem:///'
+CACHE_BACKEND = 'file:///var/www/ccdb/cache/'
 JOHNNY_MIDDLEWARE_KEY_PREFIX='jc_ccdb'
