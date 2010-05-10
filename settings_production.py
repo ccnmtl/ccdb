f42148f85b8529745121b8c2ff31f5abde8c3ec3
Anders Pearson <anders@columbia.edu>
2010-05-10 10:41:42 -0400

    try to take johnny cache to prod to see if it speeds things up

--- settings_production.py
+++ settings_production.py
@@ -8,4 +8,6 @@ MEDIA_ROOT = '/var/www/ccdb/uploads/'
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
-CACHE_BACKEND = 'locmem:///'
+#CACHE_BACKEND = 'locmem:///'
+CACHE_BACKEND = 'johnny.backends.locmem:///'
+JOHNNY_MIDDLEWARE_KEY_PREFIX='jc_ccdb'
