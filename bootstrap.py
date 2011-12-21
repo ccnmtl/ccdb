cc9a8b0364effdc6788d5731058f55b6ea715611
Anders Pearson <anders@columbia.edu>
2011-12-21 15:30:16 -0500

    convert to --no-site-packages

--- bootstrap.py
+++ bootstrap.py
@@ -4,15 +4,28 @@ import sys
 import subprocess
 import shutil
 
-pwd = os.path.dirname(__file__)
-vedir = os.path.join(pwd,"ve")
+pwd = os.path.abspath(os.path.dirname(__file__))
+vedir = os.path.abspath(os.path.join(pwd,"ve"))
 
 if os.path.exists(vedir):
     shutil.rmtree(vedir)
 
-subprocess.call([os.path.join(pwd,"pip.py"),"install",
-                 "-E",os.path.join(pwd,"ve"),
-                 "--enable-site-packages",
-                 "--requirement",os.path.join(pwd,"requirements/apps.txt")])
+virtualenv_support_dir = os.path.abspath(os.path.join(pwd, "requirements", "virtualenv_support"))
+
+ret = subprocess.call(["python", "virtualenv.py", 
+                       "--extra-search-dir=%s" % virtualenv_support_dir,
+                       "--never-download",
+                       vedir])
+if ret: exit(ret)
+
+ret = subprocess.call([os.path.join(vedir, 'bin', 'pip'), "install",
+                       "-E", vedir,
+                       "--index-url=''",
+                       "--requirement",os.path.join(pwd,"requirements/apps.txt")])
+if ret: exit(ret)
+
+ret = subprocess.call(["python","virtualenv.py","--relocatable",vedir])
+# --relocatable always complains about activate.csh, which we don't really
+# care about. but it means we need to ignore its error messages
 
 
