0bbe45b704369d7ec1e4885797076693be262a26
James Davis <jdavis@dyn-128-59-159-0.dyn.columbia.edu>
2012-03-28 10:09:20 -0400

    adding autocomplete.js

---
+++ media/js/autocomplete.js
@@ -0,0 +1,32 @@
+//the key listener
+var searchField = jQuery('#offense_text');
+searchField.bind('keyup', autoComplete);
+
+//the event handler 
+function autoComplete(e){
+	//determine if IE is in the mix
+	if (e.target){
+		var node = e.target;
+	}else if (e.srcElement) {
+		var node = e.srcElement; 
+	}
+	var val = node.value;
+	var jqxhr = jQuery.ajax({
+		type: "GET",
+		url: "/autocomplete?q=" + val,
+		datatype: "JSON"
+    })
+    .success(function() {  
+    	var searchTags = jQuery.parseJSON(jqxhr.responseText);
+    	jQuery("#offense_text").autocomplete({
+			source: searchTags,
+			minLength: 3
+		});
+    })
+    .error(function() { })
+    .complete(function() { })
+}
+
+
+
+
