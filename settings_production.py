590d76e4cbee093a06ffd0f1ad16bd268fb04b2d
Anders Pearson <anders@columbia.edu>
2012-11-27 11:50:49 -0500

    new style database config

--- settings_production.py
+++ settings_production.py
@@ -13,6 +13,17 @@ TEMPLATE_DEBUG = DEBUG
 CACHE_BACKEND = 'file:///var/www/ccdb/cache/'
 JOHNNY_MIDDLEWARE_KEY_PREFIX='jc_ccdb'
 
+DATABASES = {
+    'default' : {
+        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
+        'NAME' : 'ccdb',
+        'HOST' : '',
+        'PORT' : 6432,
+        'USER' : '',
+        'PASSWORD' : '',
+        }
+}
+
 try:
     from local_settings import *
 except ImportError:
