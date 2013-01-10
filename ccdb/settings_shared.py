68ae1e2206f314abc7232075f48c60a1d948b18f
Anders Pearson <anders@columbia.edu>
2013-01-10 15:10:22 -0500

    add django debug toolbar

--- ccdb/settings_shared.py
+++ ccdb/settings_shared.py
@@ -72,6 +72,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
     'django.middleware.transaction.TransactionMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'johnny.middleware.LocalStoreClearMiddleware',
     'johnny.middleware.QueryCacheMiddleware', )
 
@@ -103,7 +104,22 @@ INSTALLED_APPS = (
     'django_statsd',
     'django_nose',
     'smoketest',
+    'debug_toolbar',
 )
+
+INTERNAL_IPS = ('127.0.0.1', )
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+)
+
 
 STATSD_CLIENT = 'statsd.client'
 STATSD_PREFIX = 'ccdb'
