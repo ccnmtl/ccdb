64bc82bfd0d6c1366703b357fd6acb0c3ad7a4e6
Anders Pearson <anders@columbia.edu>
2013-03-22 14:44:01 -0400

    add staging config

---
+++ ccdb/settings_staging.py
@@ -0,0 +1,55 @@
+# flake8: noqa
+from settings_shared import *
+
+TEMPLATE_DIRS = (
+    "/var/www/ccdb/ccdb/ccdb/templates",
+)
+
+
+MEDIA_ROOT = '/var/www/ccdb/uploads/'
+COMPRESS_ROOT = '/var/www/ccdb/ccdb/media/'
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+STATSD_PREFIX = 'ccdb-staging'
+
+JOHNNY_MIDDLEWARE_KEY_PREFIX='jc_ccdb'
+
+CACHES = {
+    'default': dict(
+        BACKEND='johnny.backends.filebased.FileBasedCache',
+        LOCATION='file:///var/www/ccdb/cache/',
+        JOHNNY_CACHE=True,
+    )
+}
+
+
+DATABASES = {
+    'default' : {
+        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
+        'NAME' : 'ccdb',
+        'HOST' : '',
+        'PORT' : 6432,
+        'USER' : '',
+        'PASSWORD' : '',
+        }
+}
+
+if 'migrate' not in sys.argv:
+    SENTRY_SITE = 'ccdb-staging'
+    SENTRY_SERVERS = ['http://sentry.ccnmtl.columbia.edu/sentry/store/']
+
+    import logging
+    from raven.contrib.django.handlers import SentryHandler
+    logger = logging.getLogger()
+    # ensure we havent already registered the handler
+    if SentryHandler not in map(type, logger.handlers):
+        logger.addHandler(SentryHandler())
+        logger = logging.getLogger('sentry.errors')
+        logger.propagate = False
+        logger.addHandler(logging.StreamHandler())
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
