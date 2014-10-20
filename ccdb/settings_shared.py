a233cab3ee305b52769f047eb66b6118b6c322ae
Anders Pearson <anders@columbia.edu>
2014-10-20 17:05:12 +0200

    CAS conversion. PMT #95677

--- ccdb/settings_shared.py
+++ ccdb/settings_shared.py
@@ -80,6 +80,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'stagingcontext.staging_processor',
     'gacontext.ga_processor',
     'django.core.context_processors.static',
+    'djangowind.context.context_processor',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -172,10 +173,9 @@ STATICFILES_FINDERS = (
 
 # WIND settings
 
-AUTHENTICATION_BACKENDS = ('djangowind.auth.WindAuthBackend',
+AUTHENTICATION_BACKENDS = ('djangowind.auth.SAMLAuthBackend',
                            'django.contrib.auth.backends.ModelBackend',)
-WIND_BASE = "https://wind.columbia.edu/"
-WIND_SERVICE = "cnmtl_full_np"
+CAS_BASE = "https://cas.columbia.edu/"
 WIND_PROFILE_HANDLERS = ['djangowind.auth.CDAPProfileHandler']
 WIND_AFFIL_HANDLERS = ['djangowind.auth.AffilGroupMapper',
                        'djangowind.auth.StaffMapper',
