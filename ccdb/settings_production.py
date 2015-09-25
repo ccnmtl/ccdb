de9624f5393653941aedb8ddc020b387617267b5
Anders Pearson <anders@columbia.edu>
2015-09-25 09:36:52 +0200

    ccnmtlsettings

--- ccdb/settings_production.py
+++ ccdb/settings_production.py
@@ -1,18 +1,15 @@
 # flake8: noqa
 from settings_shared import *
-import os
+from ccnmtlsettings.production import common
 
-TEMPLATE_DIRS = (
-    os.path.join(os.path.dirname(__file__), "templates"),
-)
-
-
-
-MEDIA_ROOT = '/var/www/ccdb/uploads/'
-COMPRESS_ROOT = '/var/www/ccdb/ccdb/media/'
-
-DEBUG = False
-TEMPLATE_DEBUG = DEBUG
+locals().update(
+    common(
+        project=project,
+        base=base,
+        INSTALLED_APPS=INSTALLED_APPS,
+        STATIC_ROOT=STATIC_ROOT,
+        cloudfront="d3uosc1woidfa6",
+    ))
 
 CACHES = {
     'default': dict(
@@ -20,37 +17,6 @@ CACHES = {
         LOCATION='file:///var/www/ccdb/cache/',
     )
 }
-
-
-DATABASES = {
-    'default' : {
-        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
-        'NAME' : 'ccdb',
-        'HOST' : '',
-        'PORT' : 6432,
-        'USER' : '',
-        'PASSWORD' : '',
-        }
-}
-
-AWS_S3_CUSTOM_DOMAIN = "d3uosc1woidfa6.cloudfront.net"
-AWS_STORAGE_BUCKET_NAME = "ccnmtl-ccdb-static-prod"
-AWS_PRELOAD_METADATA = True
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-# static data, e.g. css, js, etc.
-S3_URL = 'https://%s/' % AWS_S3_CUSTOM_DOMAIN
-STATIC_URL = 'https://%s/media/' % AWS_S3_CUSTOM_DOMAIN
-STATICFILES_STORAGE = 'cacheds3storage.CompressorS3BotoStorage'
-COMPRESS_ENABLED = True
-COMPRESS_OFFLINE = True
-COMPRESS_ROOT = STATIC_ROOT
-COMPRESS_URL = STATIC_URL
-COMPRESS_STORAGE = 'cacheds3storage.CompressorS3BotoStorage'
-# uploaded images
-MEDIA_URL = 'https://%s.s3.amazonaws.com/uploads/' % AWS_STORAGE_BUCKET_NAME
-
-if 'migrate' not in sys.argv:
-    INSTALLED_APPS.append('raven.contrib.django.raven_compat')
 
 try:
     from local_settings import *
--- ccdb/settings_shared.py
+++ ccdb/settings_shared.py
@@ -1,50 +1,14 @@
 # Django settings for ccdb project.
 import os.path
 import sys
+from ccnmtlsettings.shared import common
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+project = 'ccdb'
+base = os.path.dirname(__file__)
 
-ADMINS = ()
+locals().update(common(project=project, base=base))
 
-MANAGERS = ADMINS
-
-ALLOWED_HOSTS = [
-    '.ccnmtl.columbia.edu',
-    '.law.columbia.edu',
-    'localhost']
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ccdb',
-        'HOST': '',
-        'PORT': 5432,
-        'USER': '',
-        'PASSWORD': '',
-        'ATOMIC_REQUESTS': True,
-    }
-}
-
-if 'test' in sys.argv or 'jenkins' in sys.argv:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': ':memory:',
-            'HOST': '',
-            'PORT': '',
-            'USER': '',
-            'PASSWORD': '',
-            'ATOMIC_REQUESTS': True,
-        }
-    }
-
-TEST_RUNNER = 'django.test.runner.DiscoverRunner'
-
-JENKINS_TASKS = (
-    'django_jenkins.tasks.run_pep8',
-    'django_jenkins.tasks.run_pyflakes',
-)
+ALLOWED_HOSTS += ['.law.columbia.edu']  # noqa
 
 PROJECT_APPS = ['ccdb.law', ]
 
@@ -55,128 +19,20 @@ CACHES = {
     )
 }
 
-TIME_ZONE = 'America/New_York'
-LANGUAGE_CODE = 'en-us'
-SITE_ID = 1
-USE_I18N = False
-MEDIA_ROOT = "/var/www/ccdb/uploads/"
-MEDIA_URL = '/uploads/'
-ADMIN_MEDIA_PREFIX = '/media/'
-SECRET_KEY = 'dummy-key-)ng#)ef_u@_^zvvu@dxm7ql-yb^_!a6%v3v^j3b(mp+)l+5%@h'
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-)
-
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth',
-    'django.template.context_processors.debug',
-    'django.template.context_processors.request',
-    'stagingcontext.staging_processor',
-    'gacontext.ga_processor',
-    'django.template.context_processors.static',
-    'djangowind.context.context_processor',
-)
-
-MIDDLEWARE_CLASSES = [
-    'django_statsd.middleware.GraphiteRequestTimingMiddleware',
-    'django_statsd.middleware.GraphiteMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
-    'impersonate.middleware.ImpersonateMiddleware',
-    'waffle.middleware.WaffleMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-]
-
-ROOT_URLCONF = 'ccdb.urls'
-
-TEMPLATE_DIRS = (
-    "/var/www/ccdb/templates/",
-    os.path.join(os.path.dirname(__file__), "templates"),
-)
-
-INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.flatpages',
-    'django.contrib.admin',
+INSTALLED_APPS += [  # noqa
     'ccdb.law',
-    'compressor',
-    'django_statsd',
-    'smoketest',
-    'debug_toolbar',
-    'django_jenkins',
-    'waffle',
-    'impersonate',
-    'django_markwhat',
-    'django.contrib.staticfiles',
-    'gunicorn',
 ]
-
-INTERNAL_IPS = ('127.0.0.1', )
-DEBUG_TOOLBAR_PANELS = (
-    'debug_toolbar.panels.version.VersionDebugPanel',
-    'debug_toolbar.panels.timer.TimerDebugPanel',
-    'debug_toolbar.panels.headers.HeaderDebugPanel',
-    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-    'debug_toolbar.panels.template.TemplateDebugPanel',
-    'debug_toolbar.panels.sql.SQLDebugPanel',
-    'debug_toolbar.panels.signals.SignalDebugPanel',
-)
-
-
-STATSD_CLIENT = 'statsd.client'
-STATSD_PREFIX = 'ccdb'
-STATSD_HOST = 'localhost'
-STATSD_PORT = 8125
 
 if 'test' in sys.argv or 'jenkins' in sys.argv:
-    STATSD_HOST = '127.0.0.1'
     CACHES = {
         'default': dict(
             BACKEND='django.core.cache.backends.dummy.DummyCache',
         )
     }
 
-COMPRESS_URL = "/media/"
-COMPRESS_ROOT = "media/"
-AWS_QUERYSTRING_AUTH = False
-
-THUMBNAIL_SUBDIR = "thumbs"
-EMAIL_SUBJECT_PREFIX = "[ccdb] "
-EMAIL_HOST = 'localhost'
-SERVER_EMAIL = "ccdb@ccnmtl.columbia.edu"
-
-STATIC_URL = "/media/"
-STATICFILES_DIRS = (
-    os.path.abspath(os.path.join(os.path.dirname(__file__), "../media/")),
-)
-STATIC_ROOT = "/tmp/ccdb/static"
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
-)
-
-
-# WIND settings
-
-AUTHENTICATION_BACKENDS = ('djangowind.auth.SAMLAuthBackend',
-                           'django.contrib.auth.backends.ModelBackend',)
-CAS_BASE = "https://cas.columbia.edu/"
-WIND_PROFILE_HANDLERS = ['djangowind.auth.CDAPProfileHandler']
-WIND_AFFIL_HANDLERS = ['djangowind.auth.AffilGroupMapper',
-                       'djangowind.auth.StaffMapper',
-                       'djangowind.auth.SuperuserMapper']
-WIND_STAFF_MAPPER_GROUPS = ['tlc.cunix.local:columbia.edu', 'caj5',
-                            'av2285', 'alm2165', 'pld2109', 'kac2160']
-WIND_SUPERUSER_MAPPER_GROUPS = ['anp8', 'jb2410', 'zm4', 'sbd12', 'egr2107',
-                                'kmh2124', 'sld2131', 'amm8', 'mar227',
-                                'ed2198']
+WIND_STAFF_MAPPER_GROUPS += [  # noqa
+    'caj5', 'av2285', 'alm2165', 'pld2109', 'kac2160'
+]
 
 # TinyMCE settings
 
@@ -193,13 +49,3 @@ TINYMCE_DEFAULT_CONFIG = {'cols': 80,
                           'plugins': 'table,spellchecker,paste,searchreplace',
                           'theme': 'simple',
                           }
-
-SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
-SESSION_COOKIE_HTTPONLY = True
-LOGIN_REDIRECT_URL = "/"
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-}
