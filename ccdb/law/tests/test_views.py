13868581d041cf7fa85888abced754c3dda9797d
Anders Pearson <anders@columbia.edu>
2016-03-18 15:54:43 +0000

    test coverage for public views

--- ccdb/law/tests/test_views.py
+++ ccdb/law/tests/test_views.py
@@ -1,19 +1,24 @@
 from django.test import TestCase
 from django.test.client import Client
-from ccdb.law.models import Snapshot
+from .factories import (
+    ChargeFactory, SnapshotFactory, ClassificationFactory,
+    AreaFactory, ConsequenceFactory,
+)
 
 
 class SimpleViewTest(TestCase):
     def setUp(self):
-        self.s = Snapshot.objects.create(
-            label="test snapshot",
-            status="vetted")
+        self.s = SnapshotFactory()
         self.c = Client()
 
     def tearDown(self):
         self.s.delete()
 
     def test_index(self):
+        response = self.c.get("/")
+        self.assertEquals(response.status_code, 200)
+
+    def test_feedback(self):
         response = self.c.get("/")
         self.assertEquals(response.status_code, 200)
 
@@ -31,9 +36,71 @@ class TestAutoComplete(TestCase):
         self.assertEquals(r.status_code, 302)
 
     def test_real(self):
-        self.s = Snapshot.objects.create(
-            label="test snapshot",
-            status="vetted")
+        self.s = SnapshotFactory()
         r = self.c.get("/autocomplete/?term=nothing")
         self.assertEquals(r.status_code, 200)
         self.assertEquals(r.content, '[]')
+
+
+class TestViewCharge(TestCase):
+    def setUp(self):
+        self.c = Client()
+
+    def test_one(self):
+        s = SnapshotFactory()
+        c = ChargeFactory(snapshot=s)
+        r = self.c.get(c.get_absolute_url())
+        self.assertEqual(r.status_code, 200)
+
+    def test_two(self):
+        s = SnapshotFactory()
+        c = ChargeFactory(snapshot=s)
+        c2 = ChargeFactory(snapshot=s)
+        r = self.c.get(
+            c.get_absolute_url() + "?charge2=" + c2.get_absolute_url())
+        self.assertEqual(r.status_code, 200)
+
+
+class TestViewChargeTips(TestCase):
+    def setUp(self):
+        self.c = Client()
+
+    def test_one(self):
+        s = SnapshotFactory()
+        c = ChargeFactory(snapshot=s)
+        r = self.c.get(c.get_absolute_url() + "tips/")
+        self.assertEqual(r.status_code, 200)
+
+
+class TestViewClassification(TestCase):
+    def setUp(self):
+        self.c = Client()
+
+    def test_view_classification(self):
+        s = SnapshotFactory()
+        c = ClassificationFactory(snapshot=s)
+        r = self.c.get(c.get_absolute_url())
+        self.assertEqual(r.status_code, 200)
+
+
+class TestViewArea(TestCase):
+    def setUp(self):
+        self.c = Client()
+
+    def test_view_aread(self):
+        s = SnapshotFactory()
+        a = AreaFactory(snapshot=s)
+        r = self.c.get(a.get_absolute_url())
+        self.assertEqual(r.status_code, 200)
+
+
+class TestViewConsequence(TestCase):
+    def setUp(self):
+        self.c = Client()
+
+    def test_view_aread(self):
+        s = SnapshotFactory()
+        a = AreaFactory(snapshot=s)
+        c = ConsequenceFactory(area=a)
+        r = self.c.get(c.get_absolute_url())
+        self.assertEqual(r.status_code, 200)
