b5603d93c33957854980d1fb46ef5aa2b8e97015
Anders Pearson <anders@columbia.edu>
2016-04-17 22:20:43 +0100

    add smoketest for the PMT URL setting

--- ccdb/law/smoke.py
+++ ccdb/law/smoke.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from smoketest import SmokeTest
 from models import Snapshot
 
@@ -6,3 +7,8 @@ class DBConnectivity(SmokeTest):
     def test_retrieve(self):
         cnt = Snapshot.objects.all().count()
         self.assertTrue(cnt > 0)
+
+
+class RequiredSettings(SmokeTest):
+    def test_pmt_url(self):
+        self.assertTrue(hasattr(settings, 'PMT_EXTERNAL_ADD_ITEM_URL'))
