266ca4409ce096eb4e5b1fd39ff0bdac4bdaa94f
Nick Buonincontri <nbuonin@gmail.com>
2019-11-08 12:43:03 -0500

    Add settings import

--- ccdb/settings_staging.py
+++ ccdb/settings_staging.py
@@ -1,15 +1,15 @@
-# flake8: noqa
-from ccdb.settings_shared import *
+from ccdb.settings_shared import *  # noqa F403
 from ccnmtlsettings.staging import common
+from django.conf import settings
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
 locals().update(
     common(
-        project=project,
-        base=base,
-        INSTALLED_APPS=INSTALLED_APPS,
-        STATIC_ROOT=STATIC_ROOT,
+        project=project,  # noqa F405
+        base=base,  # noqa F405
+        INSTALLED_APPS=INSTALLED_APPS,  # noqa F405
+        STATIC_ROOT=STATIC_ROOT,  # noqa F405
         cloudfront="d2k49vc6oxh5lr",
     ))
 
@@ -21,13 +21,13 @@ CACHES = {
 }
 
 try:
-    from ccdb.local_settings import *
+    from ccdb.local_settings import *  # noqa F403
 except ImportError:
     pass
 
 if hasattr(settings, 'SENTRY_DSN'):
     sentry_sdk.init(
-        dsn=SENTRY_DSN,
+        dsn=SENTRY_DSN,  # noqa F405
         integrations=[DjangoIntegration()],
-        debug=True,
+        debug=True
     )
